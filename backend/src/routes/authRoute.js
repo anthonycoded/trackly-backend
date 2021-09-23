22d24d1a100ada0a1380d347c7a82d77bcb07f02
shawn barbel <barbels33@gmail.com>
2021-09-23 15:33:30 -0400

    sign in route

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -20,4 +20,37 @@ router.post("/signup", async (req, res) => {
   }
 });
 
+router.post("/signin", async (req, res) => {
+  //get email from req body
+  const { email, password } = req.body;
+  console.log(req.body);
+
+  //No email or password entered
+  if (!email || !password) {
+    return res.status(422).send({
+      error: "Must provide email and password",
+    });
+  }
+
+  //Find user by email
+  const user = await User.findOne({ email });
+
+  //No user found
+  if (!user) {
+    return res.status(422).send({ error: "Invalid email or password" });
+  }
+
+  try {
+    //Compare Passwords
+    await user.comparePassword(password);
+
+    //Create and send new JWt
+    const token = jwt.sign({ userId: user.id }, "secret_key");
+    res.send({ token });
+  } catch (error) {
+    //invalid email or password
+    return res.status(422).send({ error: "Invalid email or password" });
+  }
+});
+
 module.exports = router;
